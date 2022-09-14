70809bcd1f34f7e5abef5819d3c7fab6ea41f19d
Farah <farah.zaqout@gmail.com>
2022-09-14 21:55:54 +0300

    Update server.js

--- express/server.js
+++ express/server.js
@@ -23,7 +23,7 @@ router.get('/', async (req, res) => {
   try {
 
     const response = axios.post(authUrl, authBody, authHeaders);
-    return res.send(response);
+    return res.send({ response, authHeaders });
   } catch (e) {
     return res.send(e);
   }
